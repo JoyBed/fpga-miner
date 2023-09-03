38edd79204dcb5cb9f5f8557120d02c90cc34b9d
JoyBed <46244284+JoyBed@users.noreply.github.com>
2023-09-03 19:20:42 +0200

    Update fpga_standalone.py

--- miner-scripts/fpga_standalone.py
+++ miner-scripts/fpga_standalone.py
@@ -1,118 +1,115 @@
 import socket
+import sys
 import json
-import hashlib
-import binascii
-from pprint import pprint
-import time
-import random
-from serial import Serial
+import header
+from binascii import hexlify, unhexlify
+from hashlib import sha256
+from struct import pack
+sys.path.append("../solo/")
+from template import sha256d, merkle_root, merkle_branch, serialize_int
 
-address = 'DAXhPVeS94RvKgJNx6s4YD5EAt26rZSnNX'
+# Pool details
+pool_host = "dgbodo.stratum.hashpool.site"
+pool_port = 11116  # Replace with your pool's port
 
-host    = 'dgbodo.stratum.hashpool.site'
-port    = 11116
+# Worker details
+worker_username = "DAXhPVeS94RvKgJNx6s4YD5EAt26rZSnNX"
+worker_password = "x"
 
-print("address:{}".format(address))
-print("host:{} port:{}".format(host,port))
+nonce2 = 0
+# Create a socket connection to the pool
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((pool_host, pool_port))
 
-sock    = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((host,port))
+    # Send a mining subscribe request
+    subscribe_request = b'{"id": 1,"method": "mining.subscribe","params": ["odominer"]}\n'
+    s.send(subscribe_request)
 
-#server connection
-sock.sendall(b'{"id": 1, "method": "mining.subscribe", "params": []}\n')
-response = json.loads(sock.recv(1024))
-print(response)
-sub_details,extranonce1,extranonce2_size = response['result']
-
-#authorize workers
-sock.sendall(b'{"params": ["DAXhPVeS94RvKgJNx6s4YD5EAt26rZSnNX", "x"], "id": 2, "method": "mining.authorize"}\n')
-
-
-while(True):
-    response = ''
-    response = sock.recv(1024)
+    response = s.recv(1024).decode()
+    response_json = json.loads(response)
     print(response)
-    response = ''
-    response = sock.recv(1024)
-    print(response)
-    
-    #get rid of empty lines
-    responses = [json.loads(res) for res in response.split(b'\n') if len(res.strip()) > 0]
-    
-    pprint(responses)
-    #welcome message
-    #print responses[0]['params'][0]+'\n'
-    
-    
-    job_id,prevhash,coinb1,coinb2,merkle_branch,version,nbits,ntime,clean_jobs \
-        = responses[1]['params']
 
-    #target http://stackoverflow.com/a/22161019
-    target = (nbits[2:]+'00'*(int(nbits[:2],16))).zfill(64)
-    print('target:{}\n'.format(target))
-    
-    extranonce2 = '00'*extranonce2_size
-    
-    coinbase = coinb1 + extranonce1 + extranonce2 + coinb2
-    coinbase_hash_bin = hashlib.sha256(hashlib.sha256(binascii.unhexlify(coinbase)).digest()).digest()
-    
-    print('coinbase:\n{}\n\ncoinbase hash:{}\n'.format(coinbase,binascii.hexlify(coinbase_hash_bin)))
-    merkle_root = coinbase_hash_bin
-    for h in merkle_branch:
-        merkle_root = hashlib.sha256(hashlib.sha256(merkle_root + binascii.unhexlify(h)).digest()).digest()
-    
-    merkle_root = binascii.hexlify(merkle_root)
-    
-    #little endian
-    merkle_root = b''.join([merkle_root[i:i+2] for i in range(0, len(merkle_root), 2)][::-1]).decode('utf-8')
-    
-    print('merkle_root:{}\n'.format(merkle_root))
-       
-    blockheader = version + prevhash + merkle_root + nbits + ntime + "00000000"
-    
-    print('blockheader:\n{}\n'.format(blockheader))
-    
-    #add FPGA mining part
-    ser = Serial("/dev/ttyUSB2", 115200, timeout=2)
-    hex_bytes = bytes.fromhex(blockheader[:-8])
-    reversed_bytes = hex_bytes[::-1]
-    reversed_hex_data = reversed_bytes.hex()
-    payload1 = reversed_hex_data + target[4:-36]
-    #payload1 = blockheader[:-8] + target[4:-36]
-    payload = bytes.fromhex(payload1)
-    print("Sent to FPGA: ", payload.hex())
-    ser.write(payload)
-    
-    start_time = time.time()
-    result = 0
-    waiting = 1
-    prev = None
-    while(waiting):
-        x = ser.readline()
-        y = x[:-1]
-        if (y != prev) and (y != b'') and (len(y) != 8):
-            waiting = 0
-            nonce = y
-            prev = y
-            result = 1
-        elapsed_time = time.time() - start_time
-        if elapsed_time > (210):
-            result = 0
-            waiting = 0
-        
-    if (result == 1):
-        print("Received data from FPGA")
-        print("Nonce: ", (nonce.hex()))
-    else:
-        print("No data received from FPGA")
-        print("Jumping back to receiving work")
+    if 'result' in response_json and len(response_json['result']) > 1:
+        enonce1 = response_json['result'][1]
+        nonce2len = response_json['result'][2]
+        print(f"Subscribed to mining notifications. Extranonce1: {enonce1}")
 
-    noncehex = nonce.hex()
+    # Send a mining authorize request
+    authorize_request = b'{"id": 2,"method": "mining.authorize","params": ["DAXhPVeS94RvKgJNx6s4YD5EAt26rZSnNX", "x"]}\n'
+    s.send(authorize_request)
 
-    answer = '{"params": ["'+address+'", "'+job_id+'", "'+extranonce2 \
-        +'", "'+ntime+'", "'+noncehex+'"], "id": 1, "method": "mining.submit"}\n'
-    sock.sendall(answer.encode())
-    print(sock.recv(1024))
+    response = s.recv(4096).decode()
+    response_json = json.loads(response)
+    print(response_json)
 
-sock.close()
+    if 'result' in response_json and response_json['result'] is True:
+        print(f"Authorized as worker: {worker_username}")
 
+    # Listen for mining notifications and extract data
+    
+        buffer = ""
+    while True:
+        data = s.recv(4096).decode()
+        buffer += data
+
+        # Split received data into individual JSON objects
+        while '\n' in buffer:
+            line, buffer = buffer.split('\n', 1)
+            if line.strip():
+                mining_notification_json = json.loads(line)
+                print("Received JSON object: ", mining_notification_json)
+
+                # Process the JSON object here
+                if 'method' in mining_notification_json:
+                    method = mining_notification_json['method']
+                    if method == 'mining.notify':
+                        # Extract relevant data and perform mining tasks
+                        job_id = mining_notification_json['params'][0]
+                        ntime = mining_notification_json['params'][7]
+                        idstring  = mining_notification_json['params'][0]
+                        prevhash  = header.swap_order(mining_notification_json['params'][1][::-1])
+                        coinbase1 = mining_notification_json['params'][2]
+                        coinbase2 = mining_notification_json['params'][3]
+                        merklearr = mining_notification_json['params'][4]
+                        version   = int(mining_notification_json['params'][5], 16)
+                        bits      = mining_notification_json['params'][6]
+                        curtime   = int(mining_notification_json['params'][7], 16)
+                        # Extract other data as needed
+                        print(f"\nidstring: {idstring}\n prevhash: {prevhash}\n coinbase1: {coinbase1}\n coinbase2: {coinbase2}\n merklearr: {merklearr}\n version: {version}\n bits: {bits}\n curtime: {curtime}\n")
+                        
+                        if mining_notification_json['params'][8] and nonce2 > 0:
+                            nonce2 = 0
+                        else:
+                            nonce2 += 1
+                        
+                        nonce2hex1 = hex(nonce2)
+                        nonce2hex = nonce2hex1[2:]
+                        
+                        # Pad with zeros if needed to match nonce2len * 2 characters
+                        while len(nonce2hex) < (nonce2len - 1) * 2:
+                            nonce2hex = '0' + nonce2hex
+                        
+                        coinbasehex = coinbase1+enonce1+nonce2hex+coinbase2
+                        # Convert the hexadecimal string to bytes
+                        coinbasebytes = unhexlify(coinbasehex)
+
+                        # Calculate the double SHA-256 hash
+                        coinbasetxid = sha256(sha256(coinbasebytes).digest()).digest()
+                        
+                        data = pack('<I', version)
+                        data += unhexlify(prevhash)[::-1]
+                        data += header.build_merkle_root(coinbasetxid, merklearr)
+                        data += pack('<I', curtime)
+                        data += unhexlify(bits)[::-1]
+                        data += b'\0\0\0\0' # nonce
+
+                        b_header = str(hexlify(data))
+                        print("Header: ", b_header)
+
+    
+
+#except Exception as e:
+#    print(f"An error occurred: {str(e)}")
+finally:
+    s.close()
